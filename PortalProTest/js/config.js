fdc07a60ac988a16ec0ab0152d2f7c156a4adb07
Rafael Garcia (Ariadna) <rafa@myariadna.com>
2013-05-20 18:43:30 +0200

    COPY MAY 20

---
+++ PortalProTest/js/config.js
@@ -0,0 +1,8 @@
+﻿/*-----------------------------------------------------------------------------
+config.js:
+Actúa como un fichero de configuración del aplicación. Define uan serie de v
+variables que son cargadas como objetos JSON
+------------------------------------------------------------------------------*/
+var ari_hosts = {
+    webapi: "http://localhost:56405/"
+};
