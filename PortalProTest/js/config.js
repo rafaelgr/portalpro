e75b6876760ec760ccb0b3923f51359ad0415c35
Rafael Garcia (Ariadna) <rafa@myariadna.com>
2013-05-31 13:32:06 +0200

    Copia mediodía

--- PortalProTest/js/config.js
+++ PortalProTest/js/config.js
@@ -1,6 +1,6 @@
 ﻿/*-----------------------------------------------------------------------------
 config.js:
-Actúa como un fichero de configuración del aplicación. Define uan serie de v
+Actúa como un fichero de configuración del aplicación. Define uan serie de 
 variables que son cargadas como objetos JSON
 ------------------------------------------------------------------------------*/
 var ari_hosts = {
