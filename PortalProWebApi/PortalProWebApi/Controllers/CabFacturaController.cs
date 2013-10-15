48815e54314bbadb7e041a95ee9effffcabcd6ad
Rafael Garcia (Ariadna) <rafa@myariadna.com>
2013-10-15 19:05:31 +0200

    Seguridad (15/10/2013)

--- PortalProWebApi/PortalProWebApi/Controllers/CabFacturaController.cs
+++ PortalProWebApi/PortalProWebApi/Controllers/CabFacturaController.cs
@@ -9,7 +9,7 @@ using Telerik.OpenAccess.FetchOptimization;
 
 namespace PortalProWebApi.Controllers
 {
-    public class CabFacturaController : ApiController
+    public class FacturaController : ApiController
     {
         /// <summary>
         /// Obtiene todos las cabeceras de factura de la base de datos
@@ -100,6 +100,18 @@ namespace PortalProWebApi.Controllers
                     proveedorId = factura.Proveedor.ProveedorId;
                     factura.Proveedor = null;
                 }
+                int documentoPdfId = 0;
+                if (factura.DocumentoPdf != null)
+                {
+                    documentoPdfId = factura.DocumentoPdf.DocumentoId;
+                    factura.DocumentoPdf = null;
+                }
+                int documentoXmlId = 0;
+                if (factura.DocumentoXml != null)
+                {
+                    documentoXmlId = factura.DocumentoXml.DocumentoId;
+                    factura.DocumentoXml = null;
+                }
                 // dar de alta el objeto en la base de datos y devolverlo en el mensaje
                 ctx.Add(factura);
                 if (proveedorId != 0)
@@ -108,75 +120,105 @@ namespace PortalProWebApi.Controllers
                                          where p.ProveedorId == proveedorId
                                          select p).FirstOrDefault<Proveedor>();
                 }
+                if (documentoPdfId != 0)
+                {
+                    factura.DocumentoPdf = (from d in ctx.Documentos
+                                            where d.DocumentoId == documentoPdfId
+                                            select d).FirstOrDefault<Documento>();
+                }
+                if (documentoXmlId != 0)
+                {
+                    factura.DocumentoXml = (from d in ctx.Documentos
+                                            where d.DocumentoId == documentoXmlId
+                                            select d).FirstOrDefault<Documento>();
+                }
                 ctx.SaveChanges();
-                return ctx.CreateDetachedCopy<CabFactura>(factura, x => x.Proveedor);
+                return ctx.CreateDetachedCopy<CabFactura>(factura, x => x.Proveedor, x => x.DocumentoPdf, x => x.DocumentoXml);
             }
         }
 
         /// <summary>
-        /// Modificar un usuario. En el cuerpo del mensaje se envía en el formato adecuado el objeto con los datos modificados
+        /// Modificar una cabecera de factura. En el cuerpo del mensaje se envía en el formato adecuado el objeto con los datos modificados
         /// </summary>
-        /// <param name="id"> Identificador único del grupo </param>
-        /// <param name="usuario">Grupo de usuario con los valores que se desean en sus atributos</param>
+        /// <param name="id"> Identificador único de la cabecera de factura </param>
+        /// <param name="factura">Cabecera de factura los valores que se desean en sus atributos</param>
         /// <param name="tk"> Tique de autorización (Ver 'Login')</param>
         /// <returns></returns>
-        public virtual Usuario Put(int id, Usuario usuario, string tk)
+        public virtual CabFactura Put(int id, CabFactura factura, string tk)
         {
             using (PortalProContext ctx = new PortalProContext())
             {
                 // comprobar el tique
                 if (!CntWebApiSeguridad.CheckTicket(tk, ctx))
                 {
-                    throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.Unauthorized, "Se necesita tique de autorización (Usuarios)"));
+                    throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.Unauthorized, "Se necesita tique de autorización (CabFactura)"));
                 }
                 // comprobar los formatos
-                if (usuario == null || id != usuario.UsuarioId)
+                if (factura == null || id != factura.CabFacturaId)
                 {
                     throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.BadRequest));
                 }
-                // primero buscamos si un usuario con ese id existe
-                Usuario usu = (from u in ctx.Usuarios
-                               where u.UsuarioId == id
-                               select u).FirstOrDefault<Usuario>();
+                // primero buscamos si un factura con ese id existe
+                CabFactura cfac = (from f in ctx.CabFacturas
+                                   where f.CabFacturaId == id
+                                   select f).FirstOrDefault<CabFactura>();
                 // existe?
-                if (usu == null)
+                if (cfac == null)
                 {
-                    throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.NotFound, "No hay un usuario con el id proporcionado (Usuarios)"));
+                    throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.NotFound, "No hay una factura con el id proporcionado (CabFactura)"));
                 }
-                // controlar la contraseña.
-                if (usuario.Password != null && usuario.Password != "" && usuario.Password != usu.Password)
-                {
-                    // se guarda la contraseña encriptada
-                    usuario.Password = CntWebApiSeguridad.GetHashCode(usuario.Password);
-                }
-                int grupoUsuarioId = 0;
                 // Controlamos las propiedades que son en realidad objetos.
-                if (usuario.GrupoUsuario != null)
+                int proveedorId = 0;
+                if (factura.Proveedor != null)
                 {
-                    grupoUsuarioId = usuario.GrupoUsuario.GrupoUsuarioId;
-                    usuario.GrupoUsuario = null;
+                    proveedorId = factura.Proveedor.ProveedorId;
+                    factura.Proveedor = null;
+                }
+                int documentoPdfId = 0;
+                if (factura.DocumentoPdf != null)
+                {
+                    documentoPdfId = factura.DocumentoPdf.DocumentoId;
+                    factura.DocumentoPdf = null;
+                }
+                int documentoXmlId = 0;
+                if (factura.DocumentoXml != null)
+                {
+                    documentoXmlId = factura.DocumentoXml.DocumentoId;
+                    factura.DocumentoXml = null;
                 }
                 // modificar el objeto
-                ctx.AttachCopy<Usuario>(usuario);
+                ctx.AttachCopy<CabFactura>(cfac);
                 // volvemos a leer el objecto para que lo maneje este contexto.
-                usuario = (from u in ctx.Usuarios
-                           where u.UsuarioId == id
-                           select u).FirstOrDefault<Usuario>();
-                if (grupoUsuarioId != 0)
+                cfac = (from f in ctx.CabFacturas
+                        where f.CabFacturaId == id
+                        select f).FirstOrDefault<CabFactura>();
+                if (proveedorId != 0)
                 {
-                    usuario.GrupoUsuario = (from g in ctx.GrupoUsuarios
-                                            where g.GrupoUsuarioId == grupoUsuarioId
-                                            select g).FirstOrDefault<GrupoUsuario>();
+                    factura.Proveedor = (from p in ctx.Proveedors
+                                         where p.ProveedorId == proveedorId
+                                         select p).FirstOrDefault<Proveedor>();
+                }
+                if (documentoPdfId != 0)
+                {
+                    factura.DocumentoPdf = (from d in ctx.Documentos
+                                            where d.DocumentoId == documentoPdfId
+                                            select d).FirstOrDefault<Documento>();
+                }
+                if (documentoXmlId != 0)
+                {
+                    factura.DocumentoXml = (from d in ctx.Documentos
+                                            where d.DocumentoId == documentoXmlId
+                                            select d).FirstOrDefault<Documento>();
                 }
                 ctx.SaveChanges();
-                return ctx.CreateDetachedCopy<Usuario>(usuario, x => x.GrupoUsuario);
+                return ctx.CreateDetachedCopy<CabFactura>(factura, x => x.Proveedor, x => x.DocumentoPdf, x => x.DocumentoXml);
             }
         }
 
         /// <summary>
-        /// Elimina el usuario que coincide con el id pasado
+        /// Elimina la factura que coincide con el id pasado
         /// </summary>
-        /// <param name="id">Identificador del usuario a eliminar</param>
+        /// <param name="id">Identificador de la factura a eliminar</param>
         /// <param name="tk">Tique de autorización (Ver 'Login')</param>
         /// <returns></returns>
         public virtual bool Delete(int id, string tk)
@@ -186,18 +228,18 @@ namespace PortalProWebApi.Controllers
                 // comprobar el tique
                 if (!CntWebApiSeguridad.CheckTicket(tk, ctx))
                 {
-                    throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.Unauthorized, "Se necesita tique de autorización (Usuarios)"));
+                    throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.Unauthorized, "Se necesita tique de autorización (CabFactura)"));
                 }
-                // primero buscamos si un grupo con ese id existe
-                Usuario usu = (from u in ctx.Usuarios
-                               where u.UsuarioId == id
-                               select u).FirstOrDefault<Usuario>();
+                // primero buscamos si una factura con ese id existe
+                CabFactura cfac = (from f in ctx.CabFacturas
+                                   where f.CabFacturaId == id
+                                   select f).FirstOrDefault<CabFactura>();
                 // existe?
-                if (usu == null)
+                if (cfac == null)
                 {
-                    throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.NotFound, "No hay un grupo con el id proporcionado (Usuarios)"));
+                    throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.NotFound, "No hay una factura con el id proporcionado (CabFactura)"));
                 }
-                ctx.Delete(usu);
+                ctx.Delete(cfac);
                 ctx.SaveChanges();
                 return true;
             }
---
+++ PortalProWebApi/PortalProWebApi/Controllers/LinFacturaController.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using PortalProModelo;
+using Telerik.OpenAccess.FetchOptimization;
+
+namespace PortalProWebApi.Controllers
+{
+    public class LinFacturaController : ApiController
+    {
+        /// <summary>
+        /// Devuelve las líneas que corresponden a una factura determinada
+        /// </summary>
+        /// <param name="idFac">Identificador de la factura a la que pertencen las líneas</param>
+        /// <param name="tk">Código del tique de autorización (Véase "Login")</param>
+        /// <returns></returns>
+        public virtual IEnumerable<LinFactura> GetLineas(int idFac, string tk)
+        {
+            using (PortalProContext ctx = new PortalProContext())
+            {
+                if (CntWebApiSeguridad.CheckTicket(tk, ctx))
+                {
+                    // comprobamos si existe la fcatura de referencia.
+                    CabFactura factura = (from f in ctx.CabFacturas
+                                          where f.CabFacturaId == idFac
+                                          select f).FirstOrDefault<CabFactura>();
+                    if (factura != null)
+                    {
+                        IEnumerable<LinFactura> lineas = factura.LinFacturas;
+                        FetchStrategy fs = new FetchStrategy();
+                        fs.LoadWith<LinFactura>(x => x.CabFactura);
+                        lineas = ctx.CreateDetachedCopy<IEnumerable<LinFactura>>(lineas,fs);
+                        return lineas;
+                    }
+                    else
+                    {
+                        throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.NotFound, "No hay un factura con el id proporcionado (LinFactura)"));
+                    }
+
+                }
+                else
+                {
+                    throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.Unauthorized, "Se necesita tique de autorización (LinFactura)"));
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Devuelve la línea de factura que coincide con el id proporcionado
+        /// </summary>
+        /// <param name="id">Identificador único de la línea</param>
+        /// <param name="tk">Código del tique de autorización (Véase "Login")</param>
+        /// <returns></returns>
+        public virtual LinFactura Get(int id, string tk)
+        {
+            using (PortalProContext ctx = new PortalProContext())
+            {
+                if (CntWebApiSeguridad.CheckTicket(tk, ctx))
+                {
+                    LinFactura linea = (from l in ctx.LinFacturas
+                                          where l.LinFacturaId == id
+                                          select l).FirstOrDefault<LinFactura>();
+                    if (linea != null)
+                    {
+                        linea = ctx.CreateDetachedCopy<LinFactura>(linea, x => x.CabFactura);
+                        return linea;
+                    }
+                    else
+                    {
+                        throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.NotFound, "No hay un linea con el id proporcionado (LinFactura)"));
+                    }
+                }
+                else
+                {
+                    throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.Unauthorized, "Se necesita tique de autorización (LinFactura)"));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Crear una nueva línea de factura
+        /// </summary>
+        /// <param name="LinFactura">Objeto a crear, el atributo LinFacturaId lo genera la aplicación y es devuelto en el objeto incluido en la respuesta.</param>
+        /// <param name="tk"> Tique de autorzación (se debe obtener con la accion Login)</param>
+        /// <returns></returns>
+        public virtual LinFactura Post(LinFactura linea, string tk)
+        {
+            using (PortalProContext ctx = new PortalProContext())
+            {
+                // comprobar el tique
+                if (!CntWebApiSeguridad.CheckTicket(tk, ctx))
+                {
+                    throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.Unauthorized, "Se necesita tique de autorización (LinFactura)"));
+                }
+                // comprobar las precondiciones
+                if (linea == null)
+                {
+                    throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.BadRequest));
+                }
+                int cabFacturaId = 0;
+                // Controlamos las propiedades que son en realidad objetos.
+                if (linea.CabFactura != null)
+                {
+                    cabFacturaId = linea.CabFactura.CabFacturaId;
+                    linea.CabFactura = null;
+                }
+                // dar de alta el objeto en la base de datos y devolverlo en el mensaje
+                ctx.Add(linea);
+                if (cabFacturaId != 0)
+                {
+                    linea.CabFactura = (from f in ctx.CabFacturas
+                                            where f.CabFacturaId == cabFacturaId
+                                            select f).FirstOrDefault<CabFactura>();
+                }
+                ctx.SaveChanges();
+                return ctx.CreateDetachedCopy<LinFactura>(linea, x => x.CabFactura);
+            }
+        }
+
+        /// <summary>
+        /// Modificar una linea de factura. En el cuerpo del mensaje se envía en el formato adecuado el objeto con los datos modificados
+        /// </summary>
+        /// <param name="id"> Identificador único de la linea de fcatura</param>
+        /// <param name="linea">Linea de fcatura con los valores que se desean en sus atributos</param>
+        /// <param name="tk"> Tique de autorización (Ver 'Login')</param>
+        /// <returns></returns>
+        public virtual LinFactura Put(int id, LinFactura linea, string tk)
+        {
+            using (PortalProContext ctx = new PortalProContext())
+            {
+                // comprobar el tique
+                if (!CntWebApiSeguridad.CheckTicket(tk, ctx))
+                {
+                    throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.Unauthorized, "Se necesita tique de autorización (LinFactura)"));
+                }
+                // comprobar los formatos
+                if (linea == null || id != linea.LinFacturaId)
+                {
+                    throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.BadRequest));
+                }
+                // primero buscamos si uan linea con ese id existe
+                LinFactura lin = (from l in ctx.LinFacturas
+                               where l.LinFacturaId == id
+                               select l).FirstOrDefault<LinFactura>();
+                // existe?
+                if (lin == null)
+                {
+                    throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.NotFound, "No hay una linea con el id proporcionado (LinFactura)"));
+                }
+                int cabFacturaId = 0;
+                // Controlamos las propiedades que son en realidad objetos.
+                if (lin.CabFactura != null)
+                {
+                    cabFacturaId = lin.CabFactura.CabFacturaId;
+                    linea.CabFactura = null;
+                }
+                // modificar el objeto
+                ctx.AttachCopy<LinFactura>(linea);
+                // volvemos a leer el objecto para que lo maneje este contexto.
+                linea = (from l in ctx.LinFacturas
+                           where l.LinFacturaId == id
+                           select l).FirstOrDefault<LinFactura>();
+                if (cabFacturaId != 0)
+                {
+                    linea.CabFactura = (from f in ctx.CabFacturas
+                                            where f.CabFacturaId == cabFacturaId
+                                            select f).FirstOrDefault<CabFactura>();
+                }
+                ctx.SaveChanges();
+                return ctx.CreateDetachedCopy<LinFactura>(linea, x => x.CabFactura);
+            }
+        }
+
+        /// <summary>
+        /// Elimina la linea que coincide con el id pasado
+        /// </summary>
+        /// <param name="id">Identificador de la linea a eliminar</param>
+        /// <param name="tk">Tique de autorización (Ver 'Login')</param>
+        /// <returns></returns>
+        public virtual bool Delete(int id, string tk)
+        {
+            using (PortalProContext ctx = new PortalProContext())
+            {
+                // comprobar el tique
+                if (!CntWebApiSeguridad.CheckTicket(tk, ctx))
+                {
+                    throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.Unauthorized, "Se necesita tique de autorización (LinFactura)"));
+                }
+                // primero buscamos si un grupo con ese id existe
+                LinFactura lin = (from l in ctx.LinFacturas
+                               where l.LinFacturaId == id
+                               select l).FirstOrDefault<LinFactura>();
+                // existe?
+                if (lin == null)
+                {
+                    throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.NotFound, "No hay un grupo con el id proporcionado (Usuarios)"));
+                }
+                ctx.Delete(lin);
+                ctx.SaveChanges();
+                return true;
+            }
+        }
+
+    }
+}
